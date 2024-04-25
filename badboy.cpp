d1f4edb68589064d46e17f0c5087499febd2b218
Himanshu <167037596+moonlight2909@users.noreply.github.com>
2024-04-25 21:38:22 +0530

    Add files via upload

---
+++ badboy.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, m, i, j;
+        cin >> n >> m >> i >> j;
+        cout << n << " 1 1 " << m << "\n";
+    }
+}
